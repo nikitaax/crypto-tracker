c2c3515449e8de0ebc8b31ecdbcd3cfea7a1ca23
nikitaax <nikkitanandankar@gmail.com>
2025-06-27 19:05:47 +0530

    boilerplate

---
+++ src/app/components/crypto-details/crypto-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './crypto-details.component.html',
+  styleUrl: './crypto-details.component.scss'
+})
+export class CryptoDetailsComponent {
+
+}
---
+++ src/app/components/crypto-list/crypto-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './crypto-list.component.html',
+  styleUrl: './crypto-list.component.scss'
+})
+export class CryptoListComponent {
+
+}
